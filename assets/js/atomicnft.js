c94af81bf871540d94f1044ef7f5b80abcce4668
李东樾 <dongyue@lidongyuedeMacBook-Pro.local>
2021-08-24 10:50:02 -0300

    update mobile side bar

--- assets/js/atomicnft.js
+++ assets/js/atomicnft.js
@@ -1,5 +1,12 @@
-console.log('hi there')
+console.log('Welcome to Atomic NFTs!')
 
-if(document.getElementById("nav-head").classList.contains("active")){
-    document.getElementById("child-list").style.display = "none";
-    }
+function codeaddress() {
+    document.getElementsByClassName('sidebar-about')[0].addEventListener('click', function () {
+        if ( document.getElementsByClassName('sidebar-nav')[0] && document.getElementsByClassName('sidebar-nav')[0].className && !document.getElementsByClassName('sidebar-nav')[0].className.includes('hidden') ) {
+            document.getElementsByClassName('sidebar-nav')[0].className += " hidden"
+           } else { 
+            document.getElementsByClassName('sidebar-nav')[0].className = document.getElementsByClassName('sidebar-nav')[0].className.split('hidden').join('');  
+           }
+        
+      })
+}
